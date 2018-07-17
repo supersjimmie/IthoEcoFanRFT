298441fa98d3c42bd2c8d16ff544dbd7cf34053c
bkonings <bkonings@gmail.com>
2018-07-17 12:26:37 +0200

    Update IthoEcoFanRFT.ino
    Old version created a wifi network and joined the local network.
    
    Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default, would try to act as both a client and an access-point and could cause network-issues with your other WiFi-devices on your WiFi-network.
    
    WiFi.mode(WIFI_STA);

--- IthoEcoFanRFT.ino
+++ IthoEcoFanRFT.ino
@@ -42,6 +42,12 @@ void setup(void) {
   Serial.begin(115200);
 
   // connect to wifi
+	
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+	
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
